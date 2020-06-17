7743055ee9db4230ba5a44b3c0ab06e8e4bb43b8
Viktor Cherniak <barsetca@gmail.com>
2020-06-17 13:34:03 +0300

    refactoring Restaurant repo and service

--- src/main/java/ru/cherniak/menuvotingsystem/repository/restaurant/JpaRestaurantRepository.java
+++ src/main/java/ru/cherniak/menuvotingsystem/repository/restaurant/JpaRestaurantRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.cherniak.menuvotingsystem.model.Restaurant;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 @Transactional(readOnly = true)
 public interface JpaRestaurantRepository extends JpaRepository<Restaurant, Long> {
 
--- src/main/java/ru/cherniak/menuvotingsystem/service/RestaurantService.java
+++ src/main/java/ru/cherniak/menuvotingsystem/service/RestaurantService.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.cherniak.menuvotingsystem.model.Restaurant;
-import ru.cherniak.menuvotingsystem.repository.restaurant.RestaurantRepository;
+import ru.cherniak.menuvotingsystem.repository.restaurant.JpaRestaurantRepository;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,75 +20,91 @@ import static ru.cherniak.menuvotingsystem.util.ValidationUtil.checkNotFound;
 import static ru.cherniak.menuvotingsystem.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
+@Transactional(readOnly = true)
 public class RestaurantService {
 
     protected final Logger log = LoggerFactory.getLogger(RestaurantService.class);
 
-    private final RestaurantRepository repository;
+    private static final Sort SORT_BY_NAME = Sort.by(Sort.Direction.ASC, "name");
+
+    private final JpaRestaurantRepository restaurantRepository;
 
     @Autowired
-    public RestaurantService(RestaurantRepository repository) {
-        this.repository = repository;
+    public RestaurantService(JpaRestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
     }
 
     public Restaurant get(long id) {
-        return checkNotFoundWithId(repository.get(id), id);
+        return checkNotFoundWithId(restaurantRepository.findById(id).orElse(null), id);
     }
 
+
+    private Restaurant save(Restaurant restaurant) {
+        if (!restaurant.isNew() && get(restaurant.id()) == null) {
+            return null;
+        }
+        return restaurantRepository.save(restaurant);
+    }
+
+    @Transactional
     @CacheEvict(value = "restaurants", allEntries = true)
     public Restaurant create(Restaurant restaurant) {
         Assert.notNull(restaurant, "user must not be null");
-        return repository.save(restaurant);
+        return save(restaurant);
     }
 
+    @Transactional
     @CacheEvict(value = "restaurants", allEntries = true)
     public Restaurant update(Restaurant restaurant) {
         Assert.notNull(restaurant, "user must not be null");
-        return checkNotFoundWithId(repository.save(restaurant), restaurant.getId());
+        return checkNotFoundWithId(save(restaurant), restaurant.getId());
     }
 
+    @Transactional
     @CacheEvict(value = "restaurants", allEntries = true)
     public void delete(long id) {
-        checkNotFoundWithId(repository.delete(id), id);
+        checkNotFoundWithId(restaurantRepository.deleteById(id) != 0, id);
     }
 
     public Restaurant getByName(String name) {
         Assert.notNull(name, "name must not be null");
-        return checkNotFound(repository.getByName(name), "name=" + name);
+        return checkNotFound(restaurantRepository.getByName(name).orElse(null), "name=" + name);
     }
 
     @Cacheable("restaurants")
     public List<Restaurant> getAll() {
-        return repository.getAll();
+        return restaurantRepository.findAll(SORT_BY_NAME);
     }
 
     public Restaurant getWithVotes(long id) {
-        return checkNotFoundWithId(repository.getWithVotes(id), id);
+        return checkNotFoundWithId(restaurantRepository.findOneWithVotes(id), id);
     }
 
     public Restaurant getWithDishes(long id) {
-        return checkNotFoundWithId(repository.getWithDishes(id), id);
+        return checkNotFoundWithId(restaurantRepository.findOneWithDishes(id), id);
     }
 
+
     public List<Restaurant> getAllWithDishes() {
-        return repository.getAllWithDishes();
+        return restaurantRepository.findAllWithDishes(SORT_BY_NAME);
+    }
+
+    @Cacheable("restaurants")
+    public List<Restaurant> getAllWithVotes() {
+        return restaurantRepository.findAllWithVotes();
+    }
+
+
+    public Restaurant getByNameWithVotes(String name) {
+        Assert.notNull(name, "name must not be null");
+        return checkNotFound(restaurantRepository.findOneByNameWithVotes(name).orElse(null), "name=" + name);
     }
 
     public List<Restaurant> getAllWithTodayMenu() {
-
        List<Restaurant> restaurants = getAllWithDishes();
       restaurants.forEach(r-> r.setDishes(r.getDishes().stream().filter(d ->
               d.getDate().isEqual(LocalDate.now())).collect(Collectors.toSet())));
         return restaurants;
     }
 
-    @Cacheable("restaurants")
-    public List<Restaurant> getAllWithVotes() {
-        return repository.getAllWithVotes();
-    }
-
-    public Restaurant getByNameWithVotes(String name) {
-        Assert.notNull(name, "name must not be null");
-        return checkNotFound(repository.getByNameWithVotes(name), "name=" + name);
-    }
 }
